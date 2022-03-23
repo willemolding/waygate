4c617ad1103220e89953c74805328ae8637a2c1a
Willem Olding <willemolding@gmail.com>
2022-03-23 16:09:08 +1100

    update with platformio

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,51 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 const byte DNS_PORT = 53;
 IPAddress apIP(172, 217, 28, 1);
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
 
-String responseHTML = ""
-                      "<!DOCTYPE html><html lang='en'><head>"
-                      "<meta name='viewport' content='width=device-width'>"
-                      "<title>CaptivePortal</title></head><body>"
-                      "<h1>Hello World!</h1><p>This is a captive portal example."
-                      " All requests will be redirected here.</p></body></html>";
+String siteHTML = ""
+"<!DOCTYPE html><html lang='en'><head>"
+"<meta name='viewport' content='width=device-width'>"
+"<title>WAYGATE</title></head><body>"
+"<h1>You've found something interesting!</h1>"
+"<p>This is a Waygate. Its a little place where you can leave your mark. Leave messages for people around you and read what other people have wrote.</p></body></html>"
+"";
+
+void handlePostMessage() {
+  if (webServer.method() != HTTP_POST) {
+    webServer.send(405, "text/plain", "Method Not Allowed");
+  } else {
+    // arg("plain") gets the POST body
+    webServer.send(200, "text/plain", "The new message is:\n" + webServer.arg("plain"));
+  }
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP("DNSServer CaptivePortal example");
+  WiFi.softAP("waygate");
 
-
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
 
-  // replay to all requests with same HTML
+  // reply to all requests with same HTML
   webServer.onNotFound([]() {
-    webServer.send(200, "text/html", responseHTML);
+    webServer.send(200, "text/html", siteHTML);
   });
-  webServer.begin();
 
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // the POST message response
+  webServer.on("/message/", handlePostMessage);
+
+  webServer.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  ArduinoOTA.handle();
   dnsServer.processNextRequest();
   webServer.handleClient();
 }
