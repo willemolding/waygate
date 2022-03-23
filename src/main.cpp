7e66cc8942f7168a4479c0a42bc16089fdee4c57
Willem Olding <willemolding@gmail.com>
2022-03-23 17:01:20 +1100

    switch to async web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
+#include "ESPAsyncWebServer.h"
 
 const byte DNS_PORT = 53;
 IPAddress apIP(172, 217, 28, 1);
 DNSServer dnsServer;
-ESP8266WebServer webServer(80);
+AsyncWebServer webServer(80);
 
-String siteHTML = ""
+const char siteHTML[] PROGMEM = 
 "<!DOCTYPE html><html lang='en'><head>"
 "<meta name='viewport' content='width=device-width'>"
 "<title>WAYGATE</title></head><body>"
 "<h1>You've found something interesting!</h1>"
-"<p>This is a Waygate. Its a little place where you can leave your mark. Leave messages for people around you and read what other people have wrote.</p></body></html>"
-"";
+"<p>This is a Waygate. Its a little place where you can leave your mark. Leave messages for people around you and read what other people have wrote.</p></body></html>";
 
-void handlePostMessage() {
-  if (webServer.method() != HTTP_POST) {
-    webServer.send(405, "text/plain", "Method Not Allowed");
-  } else {
-    // arg("plain") gets the POST body
-    webServer.send(200, "text/plain", "The new message is:\n" + webServer.arg("plain"));
-  }
-}
+// void saveMessage() {
+
+// }
+
+// char** getMessages() {
+
+// }
+
 
 void setup() {
   WiFi.mode(WIFI_AP);
@@ -34,12 +33,14 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", apIP);
 
   // reply to all requests with same HTML
-  webServer.onNotFound([]() {
-    webServer.send(200, "text/html", siteHTML);
+  webServer.onNotFound([](AsyncWebServerRequest *request) {
+    request->send(200, "text/html", siteHTML);
   });
 
   // the POST message response
-  webServer.on("/message/", handlePostMessage);
+  webServer.on("/message/", HTTP_POST, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "The new message is:\n" + request->arg("plain"));
+  });
 
   webServer.begin();
   Serial.println("HTTP server started");
@@ -47,5 +48,4 @@ void setup() {
 
 void loop() {
   dnsServer.processNextRequest();
-  webServer.handleClient();
 }
