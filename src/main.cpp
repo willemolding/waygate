73b2225f3d25d319c4b1752a84af3b3ec65f5b80
Willem Olding <willemolding@gmail.com>
2022-04-02 12:31:58 +1100

    add appending messages to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
+#include <LittleFS.h>
 #include "ESPAsyncWebServer.h"
 
 #include <PageSource.h>
@@ -13,6 +14,7 @@ void setup() {
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("waygate");
+  LittleFS.begin();
 
   // if DNSServer is started with "*" for domain name, it will reply with
   // provided IP to all DNS request
@@ -25,7 +27,19 @@ void setup() {
 
   // the POST message response
   webServer.on("/message/", HTTP_POST, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", "The new message is:\n" + request->arg("plain"));
+    const String from = request->getParam("from", true)->value();
+    const String body = request->getParam("body", true)->value();
+    const String timestamp = request->getParam("timestamp", true)->value();
+
+    // write this to storagae
+    File f = LittleFS.open("/messages.csv", "a");
+    if (f) {
+      f.println(from + "\037" + body + "\037" + timestamp);
+    } else {
+      Serial.println("file open failed");
+    }
+
+    request->redirect("/");
   });
 
   webServer.begin();
